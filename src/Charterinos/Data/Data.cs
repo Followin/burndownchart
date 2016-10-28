2bda6772864e7e69bf6af1349d69014e80d4878a
Pavlo Yeremenko <Pavlo_Yeremenko@epam.com>
2016-10-28 19:19:43 +0300

    Init

---
+++ src/Charterinos/Data/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Charterinos.Data
+{
+	public static class Data
+	{
+		public static Dictionary<string, Dictionary<DateTime, int>> Burndown => new Dictionary<string, Dictionary<DateTime, int>>
+		{
+			["First"] = new Dictionary<DateTime, int>
+			{
+				[DateTime.Now] = 15,
+				[DateTime.Now.AddDays(5)] = 10,
+				[DateTime.Now.AddDays(10)] = 5
+			},
+
+			["Second"] = new Dictionary<DateTime, int>
+			{
+				[DateTime.Now] = 12,
+				[DateTime.Now.AddDays(3)] = 10,
+				[DateTime.Now.AddDays(7)] = 3
+			} 
+		};
+	}
+}
---
+++ src/Charterinos/wwwroot/js/site.js
@@ -0,0 +1,114 @@
+﻿$(function() {
+    var canvas = $("#chart");
+    var ctx = canvas[0];
+    Date.MIN_VALUE = new Date(-8640000000000000);
+    Date.MAX_VALUE = new Date(8640000000000000);
+
+    $.ajax({
+            type: "GET",
+            url: canvas.data("url")
+        })
+        .then(function(data) {
+            var i, j;
+            var result = [],
+                maxValue = Number.MIN_VALUE,
+                minDate = Date.MAX_VALUE,
+                maxDate = Date.MIN_VALUE;
+
+            var dataKeys = Object.getOwnPropertyNames(data);
+            for (i = 0; i < dataKeys.length; i++) {
+                var chartDict = data[dataKeys[i]];
+                var chartDictKeys = Object.getOwnPropertyNames(chartDict);
+                var chartData = [];
+                for (j = 0; j < chartDictKeys.length; j++) {
+                    var date = new Date(chartDictKeys[j]);
+                    var value = chartDict[chartDictKeys[j]];
+
+                    chartData.push({
+                        x: date,
+                        y: value
+                    });
+
+                    if (date > maxDate) {
+                        maxDate = date;
+                    } else if (date < minDate) {
+                        minDate = date;
+                    }
+
+                    if (value > maxValue) {
+                        maxValue = value;
+                    }
+                }
+                result.push({
+                    label: dataKeys[i],
+                    data: chartData,
+                    borderColor: "#" + random.hexColor(),
+                    fill: false
+                });
+            }
+
+            console.log(minDate, maxDate, maxValue);
+
+            result.push({
+                label: "Ideal",
+                borderColor: "#" + random.hexColor(),
+                fill: false,
+                data: [
+                    {
+                        x: minDate,
+                        y: maxValue
+                    },
+                    {
+                        x: maxDate,
+                        y: 0
+                    }
+                ]
+            });
+
+
+            var chart = new Chart(ctx,
+            {
+                type: 'line',
+                data: {
+                    datasets: result
+                },
+                options: {
+                    maintainAspectRatio: true,
+                    responsive: false,
+                    scales: {
+                        xAxes: [
+                            {
+                                type: 'time',
+                                position: 'bottom',
+                                time: {
+                                    unit: "day"
+                                }
+                            }
+                        ]
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy'
+                    },
+                    zoom: {
+                        enabled: true,
+                        mode: 'xy'
+                    }
+                }
+            });
+        });
+});
+
+var random = {
+    number: function(min, max) {
+        return Math.floor(Math.random() * max) + min;
+    },
+    hexColor: function() {
+        var color = 
+          random.number(0, 255).toString(16) 
+          + random.number(0, 255).toString(16) 
+          + random.number(0, 255).toString(16);  
+
+        return color;
+    }
+}
